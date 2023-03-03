6b237bad9a02a3726981583c0aa1308508f4bce9
Brvnaxs <xavierbruna9@gmail.com>
2023-03-03 15:26:57 -0300

    reformulando projeto

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-const botao = document.querySelector('button')
-botao.addEventListener('click', ()=>{
+const body = document.body
+window.addEventListener("load", e =>{
+    e.preventDefault()
     fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
     .then((i) =>{
-        const advice = document.querySelector('.advice')
-        const txt = `<p>${i.slip.advice}</p>`
-        advice.innerHTML =  txt;
+        const adviceNumber = document.querySelector('.advice-number')
+        const number = document.createTextNode(i.slip.id)
+        const adviceContent = document.querySelector('.advice-content')
+        const advice = document.createTextNode(`"${i.slip.advice}"`)
+        adviceNumber.appendChild(number)
+        adviceContent.appendChild(advice)
+
     })
 })
+
